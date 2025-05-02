3fc09f30905c95af4612e50b051c40f69f507d5e
sujata-saini <sainisujata303@gmail.com>
2025-05-02 19:25:26 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: process.env.NODE_ENV === 'production' ? '/weatherApp/' : './',
+//   plugins: [react()],
+
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,7 +15,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/weatherApp/' : './',
   plugins: [react()],
-
 })
 
 
